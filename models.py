a48dbdf743ce687bf68ab9567957e00c7db4cfa1
Curiouspaul1 <170408025@live.unilag.edu.ng>
2020-12-13 01:53:19 +0100

    changed user models

--- models.py
+++ models.py
@@ -2,14 +2,16 @@ from sqlalchemy import (
     create_engine,
     Column, Integer,
     String, ForeignKey, Text,
-    Float
+    Float, Boolean
 )
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, sessionmaker
 Base = declarative_base()
 engine = create_engine(
     "sqlite:////home/paul/Documents/projects/smebot/dev.db"
 )
+Session = sessionmaker(bind=engine)
+session = Session()
 
 
 class Business(Base):
@@ -20,27 +22,20 @@ class Business(Base):
     telephone = Column(String(20))
     email = Column(String(50), unique=True)
     category_id = Column(Integer, ForeignKey('category.id'))
-    owner_id = Column(Integer, ForeignKey('smeowner.id'))
+    owner_id = Column(Integer, ForeignKey('user.id'))
     product = relationship("Product", back_populates="sme")
-    owner = relationship("SmeOwner", back_populates="sme")
+    owner = relationship("User", back_populates="sme")
     category = relationship("Category", back_populates="sme")
 
 
-class Customer(Base):
-    __tablename__ = 'customer'
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     name = Column(String(20))
-    email = Column(String(50), unique=True)
+    is_smeowner = Column(Boolean, default=False)
+    email = Column(String(80), unique=True)
     telephone = Column(String(20))
     preference = Column(String(50))
-
-
-class SmeOwner(Base):
-    __tablename__ = 'smeowner'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(20))
-    email = Column(String(50), unique=True)
-    telephone = Column(String(20))
     sme = relationship("Business", uselist=False, back_populates="owner")
 
 
@@ -49,6 +44,19 @@ class Category(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(50))
     sme = relationship("Business", back_populates="category")
+
+    @staticmethod
+    def add_categories():
+        cats = [
+            'Clothing/Fashion', 'Hardware Accessories',
+            'Food/Kitchen Ware', 'ArtnDesign'
+        ]
+        for i in cats:
+            results = []
+            if i not in [j.name for j in Category.query.all()]:
+                results.append(Category(name=i))
+        session.add_all(results)
+        session.commit()
 
 
 class Product(Base):
