a9d51764bfe1b29a3cd285f296750ab4c3644f94
Curiouspaul1 <170408025@live.unilag.edu.ng>
2020-12-13 01:12:56 +0100

    updated models

--- models.py
+++ models.py
@@ -1,7 +1,8 @@
 from sqlalchemy import (
     create_engine,
     Column, Integer,
-    String, ForeignKey
+    String, ForeignKey, Text,
+    Float
 )
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
@@ -19,8 +20,10 @@ class Business(Base):
     telephone = Column(String(20))
     email = Column(String(50), unique=True)
     category_id = Column(Integer, ForeignKey('category.id'))
-    category = relationship("Category", back_populates="smes")
     owner_id = Column(Integer, ForeignKey('smeowner.id'))
+    product = relationship("Product", back_populates="sme")
+    owner = relationship("SmeOwner", back_populates="sme")
+    category = relationship("Category", back_populates="sme")
 
 
 class Customer(Base):
@@ -45,7 +48,17 @@ class Category(Base):
     __tablename__ = 'category'
     id = Column(Integer, primary_key=True)
     name = Column(String(50))
-    smes = relationship("Business", back_populates="category")
+    sme = relationship("Business", back_populates="category")
+
+
+class Product(Base):
+    __tablename__ = 'product'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    description = Column(Text)
+    price = Column(Float)
+    sme_id = Column(Integer, ForeignKey('business.id'))
+    sme = relationship("Business", back_populates="product")
 
 
 if __name__ == "__main__":
